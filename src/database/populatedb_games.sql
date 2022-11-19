d71f3ca79a7a964e25587302358619883dd265fa
bluepower9 <jarrod.leong@yahoo.com>
2022-11-18 23:45:15 -0800

    Delete src directory

--- src/database/populatedb_games.sql
+++
@@ -1,4 +0,0 @@
-BEGIN TRANSACTION;
-INSERT INTO games(game_id, user_id, word) VALUES('g1', '123abc', 'abbey'); --double letter example
-INSERT INTO games(game_id, user_id, word) VALUES('g3', '123abc', 'crane'); --second game added. 
-COMMIT;
